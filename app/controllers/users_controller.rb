477ba04ae69c0d76a550c4f63b90942f6df50f69
kharumax <kharumax@gmail.com>
2020-07-14 13:32:47 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_action :admin_user,only: [:destroy]
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in(@user)
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:success] = "Please check your email to activate your account"
+      redirect_to root_url
     else
       render "new"
     end
@@ -66,6 +66,7 @@ class UsersController < ApplicationController
 
   def setup_user
     @user = User.find_by(id: params[:id])
+    redirect_to root_url unless @user.activated?
   end
 
   def admin_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save {self.email = email.downcase}
+  attr_accessor :remember_token,:activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name,presence: true,length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,presence: true,length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},uniqueness: true
   validates :password,presence: true,length: {minimum: 6},allow_nil: true
 
-  has_secure_password
-  # 新しいレコードが出来る時のみ適用される（UPDATEでは、検証されない）
+  has_secure_password # 新しいレコードが出来る時のみ適用される（UPDATEでは、検証されない）
+
+  def downcase_email
+    self.email = email.downcase
+  end
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -20,18 +24,33 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+  def activate
+    update_attribute(:activated,true)
+    update_attribute(:activated_at,Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest,User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute,token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest,nil)
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 
 
